d7f0e6191dd42c6e993b82da41389fe99beb6089
Kenneth Samonte <kenneth.samonte@vector.co.nz>
2024-11-28 13:42:54 +1300

    added RDP to sec group

--- secgroup.tf
+++ secgroup.tf
@@ -14,12 +14,15 @@ resource "aws_security_group" "ec2-secgroup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 80
-    to_port     = 80
+
+
+ingress {
+    from_port   = 3389
+    to_port     = 3389
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
