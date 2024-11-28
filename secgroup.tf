0447034dfd48e1ba7b6a2c89b814f5c431406b5d
Kenneth Samonte <kenneth.samonte@vector.co.nz>
2024-11-28 13:35:50 +1300

    removed https on sec group

--- secgroup.tf
+++ secgroup.tf
@@ -15,12 +15,6 @@ resource "aws_security_group" "ec2-secgroup" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8443
-    to_port     = 8443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
