38863cb6bb9b4cd0ed24c6cba476ea727083a6a5
Kenneth Samonte <kenneth.samonte@vector.co.nz>
2024-11-28 13:30:12 +1300

    Added https on the security group

--- secgroup.tf
+++ secgroup.tf
@@ -15,6 +15,12 @@ resource "aws_security_group" "ec2-secgroup" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
